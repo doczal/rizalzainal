dbbd42760eb6662705d1522dbfdd763699609ba6
Rizal <doczal@gmail.com>
2019-02-05 01:06:18 +0800

    Create Hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,22 @@ import Body from '../components/body'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
+import heroImage from '../images/cubes.svg'
 import styles from './home.module.scss'
 
 const IndexPage = () => (
   <Layout>
-
+    <div className={styles.hero}>
+      <div className={styles.heroContainer}>
+        <div className={styles.mainDescription}>
+          <h1 className={styles.headline}>I am <span>Rizal Zainal</span>,<br/>Front End Developer.</h1>
+          <p className={styles.subHeadline}>The grand plan for this tutorial is to build an app that helps a staffing agency manage its stable of heroes.</p>
+        </div>
+      </div>
+    </div>
+    <Body>
+      
+    </Body>
   </Layout>
 )
 
