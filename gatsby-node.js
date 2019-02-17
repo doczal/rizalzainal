800b790f0e5538b0849c1e7c103ea6d2c8f8b9dd
Rizal <doczal@gmail.com>
2019-02-17 16:29:00 +0800

    Dynamically generate project pages from Contentful API

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,29 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+   {
+      allContentfulProject {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    result.data.allContentfulProject.edges.forEach(({ node }) => {
+      createPage({
+        path: node.slug,
+        component: path.resolve(`./src/templates/project.js`),
+        context: {
+          slug: node.slug
+        }
+      })
+    })
+  })
+}
