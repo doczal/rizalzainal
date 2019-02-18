6eb3a8b9513ca713c34618160b431a9c4ea0ec47
Rizal <doczal@gmail.com>
2019-02-18 22:55:20 +0800

    Add sub heading option to Heading component

--- src/components/heading.js
+++ src/components/heading.js
@@ -3,11 +3,21 @@ import PropTypes from 'prop-types'
 import styles from './heading.module.scss'
 import classNames from 'classnames'
 
-const Heading = ({ alt, children }) => (
-  <h1 className={classNames(styles.heading, alt ? styles.alt : null)}>{children}</h1>
-)
+const Heading = ({ alt, subHeading, children }) => {
+  if (subHeading) {
+    return (
+      <h2 className={classNames(styles.heading, alt ? styles.alt : null)}>{children}</h2>
+    )
+  } else {
+    return (
+      <h1 className={classNames(styles.heading, alt ? styles.alt : null)}>{children}</h1>
+    )
+  }
+}
 
 Heading.propTypes = {
+  alt: PropTypes.bool,
+  subHeading: PropTypes.bool,
   children: PropTypes.node.isRequired,
 }
 
