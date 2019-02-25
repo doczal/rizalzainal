fece1de839ab482c46c203524d48ad8883093fac
Rizal <doczal@gmail.com>
2019-02-25 21:49:56 +0800

    Create 404 page, clean up unused images

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,19 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import styles from './404.module.scss'
+import qMark from '../images/404.svg'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className={styles.container}>
+      <div className={styles.message}>
+        <h1><span>404</span> Not Found</h1>
+        <img className={styles.questionMark} src={qMark} alt="404"/>
+        <p>Whoops, this page does not exist.</p>
+      </div>
+    </div>
   </Layout>
 )
 
